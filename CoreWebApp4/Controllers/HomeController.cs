f1871cb1444d13d8a16e35e8cf26e3ba476628f5
Ken Schlobohm <keschlob@microsoft.com>
2021-05-11 16:55:18 -0500

    added CA3009, CA3011, and CA3012

--- CoreWebApp4/Controllers/HomeController.cs
+++ CoreWebApp4/Controllers/HomeController.cs
@@ -7,7 +7,10 @@
     using System.Collections.Generic;
     using System.Diagnostics;
     using System.Linq;
+    using System.Reflection;
+    using System.Text.RegularExpressions;
     using System.Threading.Tasks;
+    using System.Xml;
 
     public class HomeController : Controller
     {
@@ -30,6 +33,44 @@
             {
 
             }
+
+            return View();
+        }
+
+        public ActionResult Index2(string anotherInput)
+        {
+            string input = Request.Form["in"];
+            XmlDocument d = new XmlDocument();
+            XmlElement root = d.CreateElement("root");
+            d.AppendChild(root);
+
+            XmlElement allowedUser = d.CreateElement("allowedUser");
+            root.AppendChild(allowedUser);
+
+            allowedUser.InnerXml = "alice";
+
+            // If an attacker uses this for input:
+            //     some text<allowedUser>oscar</allowedUser>
+            // Then the XML document will be:
+            //     <root>some text<allowedUser>oscar</allowedUser></root>
+            root.InnerXml = input;
+
+            return View();
+        }
+
+        public ActionResult Index3(string anotherInput)
+        {
+            string input = Request.Form["in"];
+            byte[] rawAssembly = Convert.FromBase64String(input);
+            Assembly.Load(rawAssembly);
+
+            return View();
+        }
+
+        public ActionResult Index4(string anotherInput)
+        {
+            string findTerm = Request.Form["findTerm"];
+            Match m = Regex.Match(anotherInput, "^term=" + findTerm);
 
             return View();
         }
--- FrameworkWebApp1/Controllers/HomeController.cs
+++ FrameworkWebApp1/Controllers/HomeController.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
+using System.Xml;
 
 namespace FrameworkWebApp1.Controllers
 {
@@ -23,6 +26,44 @@ namespace FrameworkWebApp1.Controllers
             {
 
             }
+
+            return View();
+        }
+
+        public ActionResult Index2(string anotherInput)
+        {
+            string input = Request.Form["in"];
+            XmlDocument d = new XmlDocument();
+            XmlElement root = d.CreateElement("root");
+            d.AppendChild(root);
+
+            XmlElement allowedUser = d.CreateElement("allowedUser");
+            root.AppendChild(allowedUser);
+
+            allowedUser.InnerXml = "alice";
+
+            // If an attacker uses this for input:
+            //     some text<allowedUser>oscar</allowedUser>
+            // Then the XML document will be:
+            //     <root>some text<allowedUser>oscar</allowedUser></root>
+            root.InnerXml = input;
+
+            return View();
+        }
+
+        public ActionResult Index3(string anotherInput)
+        {
+            string input = Request.Form["in"];
+            byte[] rawAssembly = Convert.FromBase64String(input);
+            Assembly.Load(rawAssembly);
+
+            return View();
+        }
+
+        public ActionResult Index4(string anotherInput)
+        {
+            string findTerm = Request.Form["findTerm"];
+            Match m = Regex.Match(anotherInput, "^term=" + findTerm);
 
             return View();
         }
